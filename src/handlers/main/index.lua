76d52dc4d526497f220ecb9d23439c4099ed94b5
Erich Ledesma <erich.ledesma@sembo.se>
2025-07-09 09:58:09 +0000

    chore: refactor, http utils extracted from main/index

--- src/handlers/main/index.lua
+++ src/handlers/main/index.lua
@@ -1,47 +1,5 @@
-local Request = require "modules.http.request"
 local cjson = require "cjson"
 local config = require "modules.config"
-
---- @return string, table, string, string, number
-local function extract_request_metadata()
-    local http_method = ngx.var.request_method
-    local request_headers = ngx.req.get_headers()
-    local request_uri = ngx.var.request_uri
-    local request_host = request_headers["Host"] or ngx.var.host
-    local current_timestamp = os.time()
-
-    return http_method, request_headers, request_uri, request_host, current_timestamp
-end
-
---- @param method string
---- @return boolean
-local function should_read_request_body(method)
-    return method ~= "GET" and method ~= "HEAD"
-end
-
---- @param method string
---- @return string|nil
-local function get_request_body_if_needed(method)
-    if not should_read_request_body(method) then
-        return nil
-    end
-
-    ngx.req.read_body()
-    return ngx.req.get_body_data()
-end
-
-local http_method, request_headers, request_uri, request_host, current_timestamp = extract_request_metadata()
-local request_body = get_request_body_if_needed(http_method)
-
-local incoming_request = Request:new(
-    http_method,
-    request_uri,
-    request_headers,
-    request_body,
-    {},
-    request_host,
-    current_timestamp
-)
 
 local execute_upstream = require "handlers.main.upstream"
 local cache = require "handlers.main.cache"
@@ -56,6 +14,7 @@ local function execute(request)
     end)
 end
 
+local incoming_request = require "handlers.utils.http"
 local cached_or_fresh_response = execute(incoming_request)
 
 --- @param response Response
---
+++ src/handlers/utils/http.lua
@@ -0,0 +1,44 @@
+local Request = require "modules.http.request"
+
+--- @return string, table, string, string, number
+local function extract_request_metadata()
+    local http_method = ngx.var.request_method
+    local request_headers = ngx.req.get_headers()
+    local request_uri = ngx.var.request_uri
+    local request_host = request_headers["Host"] or ngx.var.host
+    local current_timestamp = os.time()
+
+    return http_method, request_headers, request_uri, request_host, current_timestamp
+end
+
+--- @param method string
+--- @return boolean
+local function should_read_request_body(method)
+    return method ~= "GET" and method ~= "HEAD"
+end
+
+--- @param method string
+--- @return string|nil
+local function get_request_body_if_needed(method)
+    if not should_read_request_body(method) then
+        return nil
+    end
+
+    ngx.req.read_body()
+    return ngx.req.get_body_data()
+end
+
+local http_method, request_headers, request_uri, request_host, current_timestamp = extract_request_metadata()
+local request_body = get_request_body_if_needed(http_method)
+
+local incoming_request = Request:new(
+    http_method,
+    request_uri,
+    request_headers,
+    request_body,
+    {},
+    request_host,
+    current_timestamp
+)
+
+return incoming_request
