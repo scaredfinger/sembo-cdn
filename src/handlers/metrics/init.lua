2a26b07f1375ad40d29a54dcdb03e88d0bd63184
Erich Ledesma <erich.ledesma@sembo.se>
2025-07-10 11:14:04 +0000

    chore: metrics system in place

--- src/handlers/metrics/init.lua
+++ src/handlers/metrics/init.lua
@@ -11,37 +11,34 @@ local metrics = require "handlers.metrics.instance"
 metrics:register_composite({
   name = "upstream_request",
   label_values = {
-    method = {"GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"},
-    cache_state = {"hit", "miss", "stale"},
+    method = { "GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS" },
+    cache_state = { "hit", "miss", "stale" },
     route = route_names
   },
   histogram_suffix = "_seconds",
   counter_suffix = "_total",
-  -- buckets = { 1, 2, 3 }
   buckets = { 0.1, 0.5, 1, 2, 5, 10, 20, 40, 120 }
 })
- 
--- metrics:register_composite({
---   name = "tag_operation",
---   label_values = {
---     operation = {"get", "set", "delete"}
---   },
---   histogram_suffix = "_seconds",
---   counter_suffix = "_total",
---   -- buckets = { 1, 2, 3 }
---   buckets = { 0.01, 0.05, 0.1, 0.5, 1, 2 }
--- })
 
--- metrics:register_composite({  
---   name = "cache_operation",
---   label_values = {
---     operation = {"get", "set", "delete"},
---     cache_name = {"default", "custom"}
---   },
---   histogram_suffix = "_seconds",
---   counter_suffix = "_total",
---   -- buckets = { 1, 2, 3 }
---   buckets = { 0.01, 0.05, 0.1, 0.5, 1, 2 }
--- })
+metrics:register_composite({
+  name = "tag_operation",
+  label_values = {
+    operation = { "get", "set", "delete" }
+  },
+  histogram_suffix = "_seconds",
+  counter_suffix = "_total",
+  buckets = { 0.01, 0.05, 0.1, 0.5, 1, 2 }
+})
+
+metrics:register_composite({
+  name = "cache_operation",
+  label_values = {
+    operation = { "get", "set", "delete" },
+    cache_name = { "redis", "s3" }
+  },
+  histogram_suffix = "_seconds",
+  counter_suffix = "_total",
+  buckets = { 0.01, 0.05, 0.1, 0.5, 1, 2 }
+})
 
 return metrics
--- src/types.lua
+++ src/types.lua
@@ -1,7 +1,7 @@
 --- @class SharedDictionary 
---- @field incr fun(self: SharedDictionary, n?: number): number
+--- @field incr fun(self: SharedDictionary, key: string, n: number): number
 --- @field set fun(self: SharedDictionary, key: string, value: any)
---- @field get fun(self: SharedDictionary, key: string): string
+--- @field get fun(self: SharedDictionary, key: string): string | number
 --- @field get_keys fun(self: SharedDictionary, n?: number): string[]
 --- @field capacity fun(self: SharedDictionary): number
 --- @field free_space fun(self: SharedDictionary): number
