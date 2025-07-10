7a357f6ca2c20085ce40e027219b3345f3aa530d
Erich Ledesma <erich.ledesma@sembo.se>
2025-07-10 09:03:42 +0000

    chore: excessive logs removed

--- src/handlers/metrics/index.lua
+++ src/handlers/metrics/index.lua
@@ -1,10 +1,6 @@
 -- Create metrics instance
 local metrics = require "handlers.metrics.instance"
 
-local cjson = require "cjson"
-ngx.log(ngx.ERR, "Metrics initialized with composite metrics")
-ngx.log(ngx.ERR, "Metrics: ", cjson.encode(ngx.shared.metrics:get_keys()))
-
 -- Set content type for Prometheus
 ngx.header["Content-Type"] = "text/plain; version=0.0.4; charset=utf-8"
 
